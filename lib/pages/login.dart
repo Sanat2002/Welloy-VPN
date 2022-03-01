0d7ad524558b188e7753f87422c550c6c601db65
rameesrms <rameesrms000@gmail.com>
2022-03-02 01:33:04 +0530

    added signup and login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:wolly_vpn/constants/constants.dart';
+import 'package:wolly_vpn/pages/signup.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: colorOne,
+        leading:Icon(Icons.menu) ,
+      ),
+      body: SafeArea(
+        child: Container(padding: EdgeInsets.all(35),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: colorOne,
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start,mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Container(child:Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Log In",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: colorTwo),),
+                Text("Let's Get Started",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22,color: Colors.white)),
+
+              ],
+            ),)
+
+              ,Container(
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 0),
+                      child: Text("User Name",style: textstyleone,),
+                    ),
+                    TextFormField(
+
+                      decoration: InputDecoration(focusColor: Colors.white,contentPadding: EdgeInsets.symmetric(horizontal:5,vertical: 0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorTwo,width:2,),
+                              borderRadius: BorderRadius.circular(10))
+
+                          ,enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorTwo,width:3,),
+                              borderRadius: BorderRadius.circular(5))),),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 0),
+                      child: Text("Password",style: textstyleone,),
+                    ),
+
+
+                    TextFormField(
+
+                      decoration: InputDecoration(contentPadding: EdgeInsets.symmetric(horizontal:5,vertical: 0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorTwo,width:2,),
+                              borderRadius: BorderRadius.circular(10)),
+
+
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorTwo,width:3,),
+                              borderRadius: BorderRadius.circular(5))),),
+                  ],
+                ),
+              ),
+
+              Column(
+                children: [
+                  SizedBox(width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: colorTwo, elevation: 1),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text("Log In", style: TextStyle(fontSize: 20)),
+                        )),
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have Account?",style: TextStyle(color: Colors.white,fontSize: 12)),
+                      TextButton(onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Register()),
+                        );
+                      }, child: Text("Sign Up",style: TextStyle(color: colorTwo,fontSize: 12),))
+                    ],
+                  ),
+
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
